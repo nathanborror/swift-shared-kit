8b99dc006d9ddbe868266d3cc4d263291b80a659
Nathan Borror <nathan@nathanborror.com>
2023-12-28 12:16:45 -0800

    with ports

--- Sources/SharedKit/Extensions/URL+.swift
+++ Sources/SharedKit/Extensions/URL+.swift
@@ -2,4 +2,6 @@ import Foundation
 
 extension URL {
     public static var localhost = URL(string: "http://localhost")!
+    public static var localhost8000 = URL(string: "http://localhost:8000")!
+    public static var localhost8080 = URL(string: "http://localhost:8080")!
 }
