c822a4649acb3af5c236eb1746827ae348b1ee19
Nathan Borror <nathan@nathanborror.com>
2024-01-04 20:11:23 -0800

    tests

--- Tests/SharedKitTests/SharedKitTests.swift
+++ Tests/SharedKitTests/SharedKitTests.swift
@@ -2,11 +2,14 @@ import XCTest
 @testable import SharedKit
 
 final class SharedKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testStringApply() throws {
+        
+        let str1 = "This is my".apply(with: " content")
+        XCTAssertEqual(str1, "This is my content")
+        
+        let nilStr: String? = nil
+        let str2 = nilStr.apply(with: "This is my content")
+        XCTAssertEqual(str2, "This is my content")
     }
 }
