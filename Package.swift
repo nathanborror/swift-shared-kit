65a295118f6e60f82f7a15832133f9acba8eccc1
Nathan Borror <nathan@nathanborror.com>
2024-01-04 21:15:48 -0800

    comma

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v16),
         .macOS(.v13),
         .watchOS(.v9),
-        .tvOS(.v16)
+        .tvOS(.v16),
     ],
     products: [
         .library(name: "SharedKit", targets: ["SharedKit"]),
