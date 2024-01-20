4bfd558ca0ec1f82e52ff2aecb9dc9e0e4e2db30
Nathan Borror <nathan@nathanborror.com>
2024-01-20 09:49:08 -0800

    closure button

---
+++ Sources/SharedKit/ClosureButton.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public class ClosureButton: UIButton {
+    private var action: (() -> Void)?
+
+    public func addAction(for event: UIControl.Event = .touchUpInside, action: @escaping () -> Void) {
+        self.action = action
+        addTarget(self, action: #selector(triggerAction), for: event)
+    }
+
+    @objc private func triggerAction() {
+        action?()
+    }
+}
