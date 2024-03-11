a7b5ae673a625f77321adf955e8134329ad17969
Nathan Borror <nathan@nathanborror.com>
2024-03-11 10:00:03 -0700

    useful extensions

--- Sources/SharedKit/Extensions/NSImage+.swift
+++ Sources/SharedKit/Extensions/NSImage+.swift
@@ -1,9 +1,7 @@
+#if os(macOS)
 import Foundation
-#if os(macOS)
 import AppKit
-#endif
 
-#if os(macOS)
 extension NSImage {
     
     public func pngData() -> Data? {
@@ -13,6 +11,33 @@ extension NSImage {
         }
         
         return bitmapImage.representation(using: .png, properties: [:])
+    }
+    
+    func resize(to targetSize: CGSize) -> Data? {
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        let scaleFactor = min(widthRatio, heightRatio)
+
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        let scaledImage = NSImage(size: scaledImageSize)
+        
+        scaledImage.lockFocus()
+        
+        NSGraphicsContext.current?.imageInterpolation = .high
+        
+        draw(in: CGRect(origin: .zero, size: scaledImageSize),
+                   from: CGRect(origin: .zero, size: size),
+                   operation: .copy,
+                   fraction: 1.0)
+        scaledImage.unlockFocus()
+
+        guard let cgImage = scaledImage.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return nil }
+        let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
+        return bitmapRep.representation(using: .png, properties: [:])
     }
 }
 #endif
