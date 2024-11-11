7a0ef4690938ace2579d222fa5da7ea7f2c4c56e
Nathan Borror <nathan@nathanborror.com>
2024-11-11 11:44:07 -0800

    sanitize and whitespace helpers

--- Sources/SharedKit/Extensions/String+.swift
+++ Sources/SharedKit/Extensions/String+.swift
@@ -28,4 +28,33 @@ extension String {
         }
         return out
     }
+
+    /// Returns a sanitized string that can safely be used in a URL or filename.
+    public func sanitized() -> String {
+        let invalidCharacters = CharacterSet(charactersIn: "\\/:*?\"<>|")
+            .union(.newlines)
+            .union(.illegalCharacters)
+            .union(.controlCharacters)
+
+        return self
+            .components(separatedBy: invalidCharacters)
+            .joined(separator: "")
+    }
+
+    /// Sanitizes the string so it can safely be used in a URL or a filename.
+    public mutating func sanitize() {
+        self = self.sanitized()
+    }
+
+    /// Returns a string with all whitespace removed from the contents of the string.
+    public func whitespaceCondenced() -> String {
+        return self.components(separatedBy: .whitespacesAndNewlines)
+            .filter { !$0.isEmpty }
+            .joined(separator: " ")
+    }
+
+    /// Removes all whitespace from the contents of the string.
+    public mutating func whitespaceCondence() {
+        self = self.whitespaceCondenced()
+    }
 }
